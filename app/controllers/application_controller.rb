702209b242e2273dacd35cd63201d30fd1e22813
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-10-15 08:37:34 -0700

    Added authentication and devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :authenticate_user! #to access application, you need to be a logged in user
 end
